26a6dcda1fabc63d6053a7367461d2c1e5930411
Trent W. Buck <trentbuck@gmail.com>
2022-08-15 10:42:12 +1000

    I can get a session cookie

--- AllocCant/__init__.py
+++ AllocCant/__init__.py
@@ -1,5 +1,5 @@
 import argparse
-from getpass import getuser as whoami
+import getpass
 
 import pypass
 import requests
@@ -7,27 +7,42 @@ import requests.auth
 
 
 def main():
+    args = parse_args()
+    login(args)
+
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.set_defaults(
-        url='https://alloc.cyber.com.au/services/json.php',
         sess=requests.Session(),
         store=pypass.PasswordStore(),
     )
     args = parser.parse_args()
     args.sess.auth = requests.auth.HTTPBasicAuth(
-        username=whoami(),
+        username=getpass.getuser(),
         password=args.store.get_decrypted_password(
-            f'{whoami()}@cyber.com.au',
+            f'{getpass.getuser()}@cyber.com.au',
             entry=pypass.EntryType.password))
+    return args
 
 
-    resp = args.sess.get(
-        args.url,
-        data={'authenticate': True,
-              'username': whoami(),
-              'password': args.store.get_decrypted_password(
-                  f'{whoami()}@alloc.cyber.com.au',
-                  entry=pypass.EntryType.password),
-              })
+def login(args):
+    # Get "alloc_test_cookie=alloc_test_cookie".
+    # The POST will send it, and get back "alloc_cookie=deadbeefdeadbeefdeadbeefdeadbeef"
+    # This avoids needing to get and pass ?sessID=X around forever.
+    login_url = 'https://alloc.cyber.com.au/login/login.php'
+    resp = args.sess.get(login_url)
     resp.raise_for_status()
-    print(resp.text)
+    resp = args.sess.post(
+        login_url,
+        data={
+            'login': 'login',   # login.php requires this!
+            'username': getpass.getuser(),
+            'password': args.store.get_decrypted_password(
+                f'{getpass.getuser()}@alloc.cyber.com.au',
+                entry=pypass.EntryType.password)})
+    resp.raise_for_status()
+    # If login worked, we get redirected to /home/home.php.
+    if resp.url == login_url:
+        raise RuntimeError('bad username/password?')
+    return resp                 # DEBUGGING
